5f0623d2f7d401dbaa5cc4de60a600c73e583a0c
Tharun <tharungowda30115@gmail.com>
2025-02-04 22:47:10 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const addEventOnelem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
