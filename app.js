4bc14a9739faee81c592a3341d0828061b047bec
Tharun <tharungowda30115@gmail.com>
2025-02-04 22:47:27 +0530

    app.js

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ const addEventOnelem = function (elem, type, callback) {
   }
 }
 
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnelem(navToggler, 'click', toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
